d356ed4f58425cbeb41a27d2214e4421175580c5
WindAsMe <542636539@qq.com>
2020-09-09 09:07:34 +0900

    1000D Comparison Experiments

---
+++ in20200828/DimensionReductionForSparse/main_interface/optimization_main.py
@@ -0,0 +1,16 @@
+from in20200828.DimensionReductionForSparse.main_interface.optimization_f.Lasso import Lasso_f
+from in20200828.DimensionReductionForSparse.main_interface.optimization_f.Normal import Normal_f
+from in20200828.DimensionReductionForSparse.main_interface.optimization_f.One import One_f
+from in20200828.DimensionReductionForSparse.main_interface.optimization_f.Random import Random_f
+
+# for i in range(10):
+#     Lasso_f.f()
+
+for i in range(9):
+    Normal_f.f()
+
+for i in range(8):
+    One_f.f()
+
+for i in range(9):
+    Random_f.f()
